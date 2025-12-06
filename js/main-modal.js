4628df4f41e53a3767b7a3277ec2858020e391a1
Polimeritas <126396958+Polimeritas@users.noreply.github.com>
2025-12-06 07:59:25 +0700

    popup1

--- js/main-modal.js
+++ js/main-modal.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     showCloseButton: true,
   }).then((x) => {
     if (x.isConfirmed) {
-      window.location.href = "https://www.whatsapp.com/channel/0029VbBFyQm9xVJc7g2OQj1d";
+      window.location.href = "https://www.instagram.com/infokanorangdalam?igsh=MTZhZ3M4Znc5aHM1bg==";
     }
   });
 });
