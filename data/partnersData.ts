bbf733a0db0e9ef3085cbbb23b771844dc4cd47d
Polimeritas <126396958+Polimeritas@users.noreply.github.com>
2026-04-17 14:11:41 +0700

    Update partnersData.ts

--- data/partnersData.ts
+++ data/partnersData.ts
@@ -3967,7 +3967,7 @@ export const partnerIndustriItems: Partner[] = [
     desc: "Partner Industri Polimeritas-Exclusive",
     subDesc: "Industri",
     image: "/img/Partner2/DS Exclusive Parfume.jpg",
-    link: "https://chat.whatsapp.com/C0InZ1RMjsT2ghPiuQSV1a?mode=gi_t"
+    link: "https://wa.me/6285214991705"
   },
   {
     id: "industri-1",
