c53c4d48cb6cd614d24fc1bfa65490a4e91047d6
Polimeritas <126396958+Polimeritas@users.noreply.github.com>
2026-04-19 00:08:10 +0700

    Update partnersData.ts

--- data/partnersData.ts
+++ data/partnersData.ts
@@ -47,9 +47,9 @@ export const partnerItems: Partner[] = [
   {
     id: "hubungi-kami",
     name: "Hubungi Kami",
-    desc: "Pasang Iklan di Polimeritas",
+    desc: "Bantu Kamu Lebih Dekat",
     subDesc: "polimeritas group",
-    image: "/img/Partner2/Product-Polimeritas.png",
+    image: "/img/Partner2/gabung di polimeritas.png",
     link: "https://api.whatsapp.com/send?phone=6285214991705&text=Hi%20Polimeritas!%20Saya%20ingin%20pasang%20iklan%20"
   },
   {
