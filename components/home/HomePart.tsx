44f5f467f5eadff23537a3822cbe47508bab69f5
Polimeritas <126396958+Polimeritas@users.noreply.github.com>
2026-05-27 21:00:49 +0700

    Update HomePart.tsx

--- components/home/HomePart.tsx
+++ components/home/HomePart.tsx
@@ -41,6 +41,13 @@ const HomePartners = () => {
             image: "/game.png",
             link: "/game/polymer-lab",
         },
+        {
+            id: "Join Us",
+            name: "Join Us",
+            subDesc: "Bergabung bersama kami",
+            image: "/Join Us.png",
+            link: "/https://docs.google.com/forms/d/e/1FAIpQLSdh2xrushDA4S9wnhQjHJxWYNqOVtpqFBYAGauejHlCkROB3A/viewform?pli=1&pli=1&fbzx=2657530912750765225",
+        },
     ];
 
     return (
